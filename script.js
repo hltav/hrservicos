25c526ffeb17cc69f07dd0833f74dfcb6843b1e5
hltav <hudsonlimatavares@gmail.com>
2022-10-22 01:16:36 -0300

    Configuaração do menu

--- script.js
+++ script.js
@@ -2,20 +2,37 @@ const btninicio = document.getElementById("btn-inicio")
 const btnmanutencao = document.getElementById("btn-manutencao")
 const btnweb = document.getElementById("btn-web")
 const btnredes = document.getElementById("btn-redes")
-const bteletrica= document.getElementById("btn-eletrica")
+const btneletrica= document.getElementById("btn-eletrica")
 const btncontato = document.getElementById("btn-contato")
-const paginas = document.getElementById("pagina")
+const paginas = document.querySelectorAll('.pagina')
 
 
-esconderPaginaSelecionada= () =>{
-    const paginaSelecionada = querySelector("selecionado")
+btninicio.addEventListener("click", () => selectMenu(btninicio.value))
+btnmanutencao.addEventListener("click", () => selectMenu(btnmanutencao.value))
+btnweb.addEventListener("click", () => selectMenu(btnweb.value))
+btnredes.addEventListener("click", () => selectMenu(btnredes.value))
+btneletrica.addEventListener("click", () => selectMenu(btneletrica.value))
+btncontato.addEventListener("click", () => selectMenu(btncontato.value))
 
-    paginaSelecionada.classList.remove("selecionado")
+function selectMenu(menu) {
+    paginas.forEach((pagina) => {
+        if(pagina.classList.value.includes(menu)){
+            pagina?.classList?.add('selecionado')
+            return
+        }
+        else{
+            pagina?.classList?.remove('selecionado')
 
+        }
+
+        })
+    
 }
 
-mostrarPagina = (indicePagina) => {
-    paginas[indicePagina].classList.add("selecionado")
 
-}
+
+
+
+
+
 
